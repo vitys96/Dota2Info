52bb839e9d00bcce880b31be07665394a3f98eb6
Kaboo <vitys96@mail.ru>
2023-03-24 21:17:42 +0300

    initial

---
+++ Dota2 info/Dota2 info/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dota2 info
+//
+//  Created by Виталий Охрименко on 24.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
